a383306d0efad7d553e8a850868ad7badc92fc69
Florent Michel <flomiche@drago5.saclay.inria.fr>
2022-07-04 14:50:49 +0200

    Cleaning code

--- solvers/dip_image_prior.py
+++ solvers/dip_image_prior.py
@@ -2,8 +2,6 @@ from benchopt import BaseSolver, safe_import_context
 from benchopt.stopping_criterion import SufficientProgressCriterion
 
 with safe_import_context() as import_ctx:
-    # import ipdb
-    import numpy as np
     import torch
 
     get_net = import_ctx.import_from("models", "get_net")
@@ -22,31 +20,44 @@ class Solver(BaseSolver):
     )
 
     # any parameter defined here is accessible as a class attribute
+    parameters = {
+        "learning_rate": [0.1],
+        "optimizer": ["SGD"],
+        "input_depth": [32],
+        "pad": ["reflection"],
+        "input_std": [0.1],
+        "reg_noise_std": [0.03],
+    }
 
     def set_objective(self, filt, A, Y, X_shape):
         # The arguments of this function are the results of the
         # `to_dict` method of the objective.
         # They are customizable.
-        self.filt, self.A, self.Y, self.X_shape = filt, A, Y.flatten(), X_shape
+        self.filt, self.A, self.Y, self.X_shape = filt, A, Y, X_shape
 
     @staticmethod
     def get_next(stop_val):
         return stop_val + 1
 
-    def run(self, callback, pad = "reflection", var = 1.0 / 10, lr = 1e-3, reg_noise_std = 1.0 / 30, input_depth = 32):
-        if torch.cuda.device_count()>0:
+    def run(
+        self,
+        callback,
+    ):
+        if torch.cuda.device_count() > 0:
             dtype = torch.cuda.FloatTensor
         else:
             dtype = torch.FloatTensor
-        
-        blur_operator = torch.nn.Conv2d(1,1,self.filt.shape,padding='same', bias=False)
+
+        blur_operator = torch.nn.Conv2d(
+            1, 1, self.filt.shape, padding="same", bias=False
+        )
         blur_operator.weight.data = np_to_torch(self.filt).type(dtype)
         blur_operator.requires_grad_(False)
 
         net = get_net(
-            input_depth,
-            "skip",
-            pad=pad,
+            input_depth=self.input_depth,
+            NET_TYPE="skip",
+            pad=self.pad,
             n_channels=1,
             skip_n33d=128,
             skip_n33u=128,
@@ -57,19 +68,27 @@ class Solver(BaseSolver):
 
         mse = torch.nn.MSELoss().type(dtype)
 
-        Y_torch = np_to_torch(self.Y.reshape(self.X_shape)).type(dtype)
+        Y_torch = np_to_torch(self.Y).type(dtype)
 
-        noise_input_saved = var * torch.rand(
-            1, input_depth, self.X_shape[0], self.X_shape[1]
+        noise_input_saved = self.input_std * torch.rand(
+            1, self.input_depth, self.X_shape[0], self.X_shape[1]
         ).type(dtype)
 
-        optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+        if self.optimizer == "SGD":
+            optimizer = torch.optim.SGD(
+                net.parameters(), lr=self.learning_rate
+            )
+        elif self.optimizer == "Adam":
+            optimizer = torch.optim.Adam(
+                net.parameters(), lr=self.learning_rate
+            )
 
         out = net(noise_input_saved)
 
         while callback(torch_to_np(out)):
-            noise_input = noise_input_saved + reg_noise_std * torch.randn_like(
+            noise_input = (
                 noise_input_saved
+                + self.reg_noise_std * torch.randn_like(noise_input_saved)
             )
             out = net(noise_input)
             loss = mse(blur_operator(out), Y_torch)
