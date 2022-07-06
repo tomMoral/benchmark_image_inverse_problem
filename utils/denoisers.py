7fa52dc2f5a83616938c9b826b1a7c7ddd1f01ab
tomMoral <thomas.moreau.2010@gmail.com>
2022-07-06 10:52:20 +0200

    CLN drunet denoiser

--- utils/denoisers.py
+++ utils/denoisers.py
@@ -1,46 +1,30 @@
 
-from benchopt import BaseSolver, safe_import_context
+from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
-    import numpy as np
-    # conv = import_ctx.import_from('basicblock', 'conv')
-    # downsample_avgpool = import_ctx.import_from('basicblock','downsample_avgpool')
-    # downsample_strideconv = import_ctx.import_from('basicblock','downsample_strideconv')
-    # sequential = import_ctx.import_from('basicblock', 'sequential')
-    # ResBlock = import_ctx.import_from('basicblock', 'ResBlock')
-    # upsample_upconv = import_ctx.import_from('basicblock', 'upsample_upconv')
-    # upsample_convtranspose = import_ctx.import_from('basicblock', 'upsample_convtranspose')
+    import requests
+    from pathlib import Path
+    from abc import ABC, abstractmethod
+
+    import bm3d
+    import torch
+    from skimage.restoration import denoise_nl_means
+
     single2tensor4 = import_ctx.import_from('basicblock', 'single2tensor4')
     uint2single = import_ctx.import_from('basicblock', 'uint2single')
-    import bm3d
-    from skimage.restoration import denoise_nl_means
-    from functools import partial
-    from abc import ABC, abstractmethod
-    import torch
-    import os
-    import imageio
-    import matplotlib.pyplot as plt
-    import requests
-    
+    UNetRes = import_ctx.import_from('basicblock', 'UNetRes')
 
 
 class BaseDenoiser(ABC):
     def __init__(self) -> None:
         super().__init__()
-        
+
     @abstractmethod
     def __call__(self, image, sigma):
         pass
 
-class BaseDenoiser_drunet(ABC):
-    def __init__(self) -> None:
-        super().__init__()
-        
-    @abstractmethod
-    def __call__(self, image, model, device):
-        pass
 
-class BM3D_Denoiser(BaseDenoiser):
+class BM3DDenoiser(BaseDenoiser):
     def __init__(self) -> None:
         super().__init__()
 
@@ -48,7 +32,7 @@ class BM3D_Denoiser(BaseDenoiser):
         return bm3d.bm3d(z=image, sigma_psd=sigma)
 
 
-class NLM_Denoiser(BaseDenoiser):
+class NLMDenoiser(BaseDenoiser):
     def __init__(self) -> None:
         super().__init__()
 
@@ -56,65 +40,75 @@ class NLM_Denoiser(BaseDenoiser):
         return denoise_nl_means(image=image, sigma=sigma)
 
 
-#  : add denoiser optionnal parameters (eg dict input) ?
+def download_pretrained_model(model_dir='model_zoo',
+                              model_name='drunet_gray.pth'):
 
-def load_denoiser(denoiser_name : str):
-    denoiser_name_list = ['bm3d', 'nlm', 'drunet_gray']
-    if denoiser_name == "bm3d":
-        return BM3D_Denoiser()
-    elif denoiser_name == "nlm":
-        return NLM_Denoiser()
-    elif denoiser_name == 'drunet_gray':
-        download_pretrained_model()
-        return model_drunet_class()
-    else:
-        raise ValueError('unknown denoiser name. Got {}, expected denoiser_name in {}'.format(denoiser_name, denoiser_name_list))
-
-
-
-
-
-
-def download_pretrained_model(model_dir='model_zoo', model_name='drunet_gray.pth'):
-    if os.path.exists(os.path.join(model_dir, model_name)):
-        print(f'already exists, skip downloading [{model_name}]')
-    else:
-        os.makedirs(model_dir, exist_ok=True)
+    model_dir = Path(model_dir)
+    model_file = model_dir / model_name
+    if not model_file.exists():
+        model_dir.mkdir(exist_ok=True)
         if 'SwinIR' in model_name:
-            url = 'https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/{}'.format(model_name)
+            url = (
+                "https://github.com/JingyunLiang/SwinIR/releases/download/"
+                f"v0.0/{model_name}"
+            )
         else:
-            url = 'https://github.com/cszn/KAIR/releases/download/v1.0/{}'.format(model_name)
+            url = (
+                "https://github.com/cszn/KAIR/releases/download/"
+                f"v1.0/{model_name}"
+            )
         r = requests.get(url, allow_redirects=True)
         print(f'downloading [{model_dir}/{model_name}] ...')
-        open(os.path.join(model_dir, model_name), 'wb').write(r.content)
+        model_file.write_bytes(r.content)
         print('done!')
 
-def model_drunet(img_L, model, device):
-    print('Run model')
-    img_L = uint2single(img_L) + np.random.normal(0, 15/255., img_L.shape)
-    img_L = img_L.reshape((img_L.shape[0],img_L.shape[1],1))
-    img_L = single2tensor4(img_L)
-    img_L = torch.cat((img_L, torch.FloatTensor([15/255.]).repeat(1, 1, img_L.shape[2], img_L.shape[3])), dim=1)
-    img_L = img_L.to(device)
-    img_E = model(img_L)
-    plt.figure()
-    plt.subplot(211)
-    plt.imshow(np.squeeze(img_L[0,0,:,:]))
-    plt.subplot(212)
-    plt.imshow(np.squeeze(img_E[0,:,:]))
-    plt.show()
-    
-    return img_E
+        return model_file
 
-class model_drunet_class(BaseDenoiser_drunet):
+
+class DrunetDenoiser(BaseDenoiser):
     def __init__(self) -> None:
         super().__init__()
 
-    def __call__(self, image, model, device):
-        return model_drunet(image, model, device)
-        
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        n_channels = 1
+
+        model_path = download_pretrained_model()
+        torch.cuda.empty_cache()
+        model = UNetRes(
+            in_nc=n_channels+1, out_nc=n_channels, nc=[64, 128, 256, 512],
+            nb=4, act_mode='R', downsample_mode="strideconv",
+            upsample_mode="convtranspose"
+        )
+        model.load_state_dict(torch.load(model_path), strict=True)
+        model = model.to(device=self.device)
+        model.eval()
+
+        self.model = model
+
+    def __call__(self, image, sigma):
+        # img_L = uint2single(image)
+        # img_L = img_L.reshape((img_L.shape[0],img_L.shape[1],1))
+        # img_L = single2tensor4(img_L)
+        # img_L = torch.cat((img_L, torch.FloatTensor([15/255.])
+        # .repeat(1, 1, img_L.shape[2], img_L.shape[3])), dim=1)
+        # img_L = img_L.to(device)
+        with torch.no_grad():
+            return self.model(image)
 
 
+#  : add denoiser optionnal parameters (eg dict input) ?
 
-
-
+def load_denoiser(denoiser_name: str):
+    denoiser_name_list = ['bm3d', 'nlm', 'drunet_gray']
+    if denoiser_name == "bm3d":
+        return BM3DDenoiser()
+    elif denoiser_name == "nlm":
+        return NLMDenoiser()
+    elif denoiser_name == 'drunet_gray':
+        return DrunetDenoiser()
+    else:
+        raise ValueError(
+            'unknown denoiser name. Got {}, expected denoiser_name in {}'
+            .format(denoiser_name, denoiser_name_list)
+        )
