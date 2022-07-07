c7e2bd3ace0c65afe21cffad44fdab48c04fffb9
jprost <jean.prost@math.u-bordeaux.fr>
2022-07-07 11:30:50 +0200

    add unsqueeze + squeeze in drunet denoiser

--- utils/denoisers.py
+++ utils/denoisers.py
@@ -92,8 +92,8 @@ class DrunetDenoiser(BaseDenoiser):
         # img_L = single2tensor4(img_L)
         # img_L = img_L.to(device)
         with torch.no_grad():
-            img_L = torch.cat([image, 15/255 * torch.ones_like(image)], dim=0)
-            return self.model(img_L)
+            img_L = torch.cat([image, 15/255 * torch.ones_like(image)], dim=0).unsqueeze(0)
+            return self.model(img_L).squeeze()
 
 
 #  : add denoiser optionnal parameters (eg dict input) ?
